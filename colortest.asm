a21b4eff0b47f721564d9d560415824069514cf2
tarhim <kzietara@gmail.com>
2025-03-29 08:55:19 +0100

    Add files via upload

---
+++ colortest.asm
@@ -0,0 +1,77 @@
+
+;test ekranu
+;Trzy najmłodsze bity (0,1,2) to ink, trzy kolejne (3,4,5) paper, ;kolejny (6) to bright a ostatni (7) to flash.
+;Czyli czarne na białym to 00 111 000, białe na czarnym z brightem ;01 000 111, mrugające niebieskie na czerwonym 10 010 001 itd.
+
+ORG 32768
+
+start
+
+
+;kolor permanent
+LD a, %00111000
+LD (23693),a
+
+CALL cls
+
+ LD a,2 ; 2 = górny ekran
+CALL 5633
+
+
+LD B,7 
+petla
+
+
+
+       ; LD B, 0                 ; Initialize B to 0 (color counter)
+kolor:   LD A, B                 ; Load current color into A
+        OUT (254), A            ; Set BORDER color (FEh is border/ULA port)
+        AND 127
+		LD(23695), a ; atrybuty tekstu
+        DEC B                   ; Increment color (0-7 for 8 colors)
+        CALL DELAY
+        PUSH BC
+
+
+
+;kolor
+;LD a, b
+;LD (23695),a
+
+LD A,255
+LD (23692), A ; scroll
+
+LD de,tekst ; adres tekstu testowego
+LD bc,koniec_tekstu - tekst
+CALL 8252
+POP BC
+
+
+
+;CALL DELAY
+
+JP petla ; nieskończona pętla
+
+DELAY:  LD DE, 00FFFh           ; Large delay value
+DELAY_LOOP:
+        DEC DE
+        LD A, D
+        OR E
+        JP NZ, DELAY_LOOP       ; Loop until DE = 0
+ RET      
+
+
+tekst 
+DEFM "TEST"
+koniec_tekstu
+
+cls:
+
+LD HL, 16384                  ; Start of screen memory
+        LD DE, 16385
+        LD BC, 6912             ; 6912 bytes for full ZX Spectrum screen
+        LD (HL), 0              ; Clear first byte
+        LDIR      
+	    RET
+	
+END start
